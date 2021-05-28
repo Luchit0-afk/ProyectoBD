6c5e18506d4aed205aa3cfa7195eede96bdc33c0
Luchit0-afk <lucianopalacio91@gmail.com>
2021-05-28 17:59:44 -0300

    Tablas hechas faltan los triggers

--- Ej3-Proyecto.sql
+++ Ej3-Proyecto.sql
@@ -1,6 +1,6 @@
 
 
-CREATE TABLE persona (
+CREATE TABLE Persona (
 DNI integer,
 CHECK (DNI > 0),
 nombre varchar(50),
@@ -9,66 +9,84 @@ direccion varchar(100),
 PRIMARY KEY (DNI)
 );
 
-CREATE TABLE alumno (
-CONSTRAINT fk_alumno FOREIGN KEY (DNI) REFERENCES persona(DNI),
-num_alumno integer,
-CONSTRAINT fk_alumno_2 FOREIGN KEY (cod_resolucion) REFERENCES resolucion(cod_resolucion)
+CREATE TABLE Docente (
+DNI_docente integer,
+dedicacion enum('exclusivo','semi-exclusivo','simple'),
+CONSTRAINT pk_docente PRIMARY KEY (DNI_docente),
+CONSTRAINT fk_docente_persona FOREIGN KEY (DNI_docente) REFERENCES Persona(DNI)
 );
 
-CREATE TABLE docente (
-CONSTRAINT fk_docente FOREIGN KEY (DNI) REFERENCES persona(DNI),
-dedicacion varchar(100)
-);
-
-CREATE TABLE actividad (
-cod_actividad integer,
-desc_actividad varchar(200),
-CONSTRAINT fk_actividad FOREIGN KEY (cod_materia) REFERENCES materia(cod_materia),
-PRIMARY KEY (cod_actividad)
-);
-
-CREATE TABLE materia (
+CREATE TABLE Materia (
 cod_materia integer,
 nombre varchar(50),
-CONSTRAINT fk_materia FOREIGN KEY (DNI) REFERENCES docente(DNI),
-PRIMARY KEY (cod_materia)
+DNI_docente integer,
+CONSTRAINT pk_materia PRIMARY KEY (cod_materia),
+CONSTRAINT fk_materia_docente FOREIGN KEY (DNI_docente) REFERENCES Docente(DNI_docente)
 );
 
-CREATE TABLE resolucion (
+CREATE TABLE Actividad (
+cod_actividad integer,
+desc_actividad varchar(200),
+cod_materia integer,
+CONSTRAINT pk_actividad PRIMARY KEY (cod_actividad),
+CONSTRAINT fk_actividad_materia FOREIGN KEY (cod_materia) REFERENCES Materia(cod_materia)
+);
+
+CREATE TABLE Resolucion (
 cod_resolucion integer,
 fecha date,
 hora integer,
-/* QUE PONER A HORA? JUNTAR CON FECHA?*/
+DNI_docente integer,
+cod_actividad integer,
 nota integer,
-CONSTRAINT fk_resolucion FOREIGN KEY (DNI) REFERENCES docente(DNI),
-CONSTRAINT fk_resolucion_2 FOREIGN KEY (cod_actividad) REFERENCES actividad(cod_actividad),
-PRIMARY KEY (cod_resolucion)
+CONSTRAINT pk_resolucion PRIMARY KEY (cod_resolucion),
+CONSTRAINT fk_resolucion_docente FOREIGN KEY (DNI_docente) REFERENCES Docente(DNI_docente),
+CONSTRAINT fk_resolucion_actividad FOREIGN KEY (cod_actividad) REFERENCES Actividad(cod_actividad)
 );
 
-CREATE TABLE facultad (
+CREATE TABLE Alumno (
+DNI_alumno integer ,
+num_alumno integer,
+cod_resolucion integer,
+CONSTRAINT pk_alumno PRIMARY KEY (DNI_alumno),
+CONSTRAINT fk_alumno_persona FOREIGN KEY (DNI_alumno) REFERENCES Persona(DNI),
+CONSTRAINT fk_alumno_resolucion FOREIGN KEY (cod_resolucion) REFERENCES Resolucion(cod_resolucion)
+);
+
+CREATE TABLE Facultad (
 cod_facultad integer,
 desc_facultad varchar(200),
-PRIMARY KEY (cod_facultad)
+CONSTRAINT pk_facultad PRIMARY KEY (cod_facultad)
 );
 
-CREATE TABLE cargo (
+CREATE TABLE Cargo (
 cod_cargo integer,
 desc_cargo varchar(200),
-PRIMARY KEY (cod_cargo)
+CONSTRAINT pk_cargo PRIMARY KEY (cod_cargo)
 );
 
-CREATE TABLE cursa (
-CONSTRAINT fk_cursa FOREIGN KEY (DNI) REFERENCES alumno(DNI),
-CONSTRAINT fk_cursa_2 FOREIGN KEY (cod_materia) REFERENCES materia(cod_materia)
+CREATE TABLE Cursa (
+DNI_alumno integer,
+cod_materia integer,
+CONSTRAINT pk_cursa PRIMARY KEY (DNI_alumno,cod_materia),
+CONSTRAINT fk_cursa_alumno FOREIGN KEY (DNI_alumno) REFERENCES Alumno(DNI_alumno),
+CONSTRAINT fk_cursa_materia FOREIGN KEY (cod_materia) REFERENCES Materia(cod_materia)
 );
 
-CREATE TABLE asignado (
-CONSTRAINT fk_asignado FOREIGN KEY (cod_materia) REFERENCES materia(cod_materia),
-CONSTRAINT fk_asignado_2 FOREIGN KEY (DNI) REFERENCES docente(DNI)
+CREATE TABLE Asignado (
+cod_materia integer,
+DNI_docente integer,
+CONSTRAINT pk_asignado PRIMARY KEY (cod_materia,DNI_docente),
+CONSTRAINT fk_asignado_materia FOREIGN KEY (cod_materia) REFERENCES Materia(cod_materia),
+CONSTRAINT fk_asignado_2 FOREIGN KEY (DNI_docente) REFERENCES Docente(DNI_docente)
 );
 
-CREATE TABLE pertenece (
-CONSTRAINT fk_pertenece FOREIGN KEY (DNI) REFERENCES docente(DNI),
-CONSTRAINT fk_pertenece_2 FOREIGN KEY (cod_facultad) REFERENCES facultad(cod_facultad),
-CONSTRAINT fk_pertenece_3 FOREIGN KEY (cod_cargo) REFERENCES cargo(cod_cargo)
+CREATE TABLE Pertenece (
+cod_facultad integer,
+DNI_docente integer,
+cod_cargo integer,
+CONSTRAINT pk_pertenece PRIMARY KEY (cod_facultad,DNI_docente),
+CONSTRAINT fk_pertenece_docente FOREIGN KEY (DNI_docente) REFERENCES Docente(DNI_docente),
+CONSTRAINT fk_pertenece_facultad FOREIGN KEY (cod_facultad) REFERENCES Facultad(cod_facultad),
+CONSTRAINT fk_pertenece_cargo FOREIGN KEY (cod_cargo) REFERENCES Cargo(cod_cargo)
 );
