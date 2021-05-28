603e174f13b31432f70fa7538836e0b877c2fb16
Luchit0-afk <lucianopalacio91@gmail.com>
2021-05-28 09:15:50 -0300

    Carga inicial

---
+++ scripCarga.sql
@@ -0,0 +1,60 @@
+--
+-- Carga de contenidos en la tablas
+--
+
+
+INSERTO INTO 'Persona' ('DNI','nombre','apellido','direccion') VALUES 
+(12543569,'Sofia','Urquiza','Tucuman 123'),
+(12345568,'Rocio','Urquiza','Tucuman 123'),
+(23543882,'Thomas','Sanchez','Mendoza 456'),
+(32556763,'Tobias','Sanchez','Mendoza 456'),
+(42465690,'Ivan','Bakanowsky','Cabrera 800'),
+(37891852,'Sabrina','Fernandez','Guemes 1020'),
+(25455069,'Franco','Fernandez','Costa Rica 800'),
+(39543576,'Braiam','Zulberti','Bariloche 13'),
+(42543564,'Franco','Arias','Bariloche 18'),
+(12547543,'Nicolas','Pereyra','Peron 754'),
+(34435690,'Guadalupe','Tortosa','Santiago del estero 645');
+(33465969,'Noelia','Urquiza','Luis Pasteur 18'),
+(39543576,'Marisa','Flores','Rafael Obligado 765'),
+(34547864,'Micaela','Lepori','Alberdi 813'),
+(29446541,'Franco','Barzola','9 de julio 81'),
+(29435690,'Matias','Barzola','San Martin 890');
+
+
+INSERTO INTO 'M-Persona' ('DNI','telefono') VALUES 
+(12543569,15447889),
+(12345568,15458776),
+(23543882,15644710),
+(32556763,15648890),
+(42465690,15478889),
+(37891852,15441823),
+(25455069,15438782),
+(39543576,15545781),
+(42543564,15646788),
+(12547543,15457685),
+(34435690,15548584);
+(33465969,15579487),
+(39543576,15671782),
+(34547864,15668381),
+(29446541,15450480),
+(29435690,15442589;
+
+
+INSERTO INTO 'Persona' ('DNI_alumno','numero_alumno','cod_resolucion') VALUES 
+(12543569,'Sofia','Urquiza','Tucuman 123'),
+(12345568,'Rocio','Urquiza','Tucuman 123'),
+(23543882,'Thomas','Sanchez','Mendoza 456'),
+(32556763,'Tobias','Sanchez','Mendoza 456'),
+(42465690,'Ivan','Bakanowsky','Cabrera 800'),
+(37891852,'Sabrina','Fernandez','Guemes 1020'),
+(25455069,'Franco','Fernandez','Costa Rica 800'),
+(39543576,'Braiam','Zulberti','Bariloche 13'),
+(42543564,'Franco','Arias','Bariloche 18'),
+(12547543,'Nicolas','Pereyra','Peron 754'),
+(34435690,'Guadalupe','Tortosa','Santiago del estero 645');
+(33465969,'Noelia','Urquiza','Luis Pasteur 18'),
+(39543576,'Marisa','Flores','Rafael Obligado 765'),
+(34547864,'Micaela','Lepori','Alberdi 813'),
+(29446541,'Franco','Barzola','9 de julio 81'),
+(29435690,'Matias','Barzola','San Martin 890');
