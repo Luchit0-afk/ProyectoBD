5784748e99506d0095ba9469c7643267f07b1058
EAMolina500 <molinaenzoagustin@gmail.com>
2021-06-09 19:50:37 -0300

    consultas

--- Consultas.sql
+++ Consultas.sql
@@ -1,5 +1,16 @@
 Use Proyecto;
 
+USE Proyecto;
+
+# ACTIVIDAD A
+# SELECT DNI_alumno FROM Alumno WHERE DNI_Alumno NOT IN (SELECT DNI_alumno FROM (Alumno NATURAL JOIN Resolucion NATURAL JOIN Actividad) WHERE cod_materia = 2)
+
+# ACTIVIDAD B
+# SELECT cod_materia FROM Materia WHERE cod_materia NOT IN (SELECT cod_materia FROM Materia UNION SELECT cod_materia FROM Asignado)
+
+# MENOS CODIGO, PERO NO ANDA
+# SELECT cod_materia FROM Materia WHERE cod_materia NOT IN (SELECT cod_materia FROM (Materia NATURAL JOIN Asignado))
+
 /*C}*/
 SELECT Alumno.DNI_alumno FROM  
 (Docente JOIN Alumno)
